345571032339c03a4ee39e55fe4a6ce17559afc4
Ben Sun <sunb26@mcmaster.ca>
2024-01-14 03:15:50 -0500

    fixed var name matching in write endpoint

--- src/crud/write.py
+++ src/crud/write.py
@@ -35,15 +35,15 @@ def new_topic(user:str, topic:str) -> bool:
     conn.close()
     return None
 
-def new_qa(user:str, topic:str, qa_list: list) -> bool:
+def new_qa(username:str, topic:str, qa_list: list) -> bool:
     conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, sslmode=sslmode)
     cur = conn.cursor()
 
     for qa in qa_list:
-        q = qa["term"]
-        a = qa["definition"]
+        q = qa["question"]
+        a = qa["answer"]
         try:
-            cur.execute("INSERT INTO qa (username, topic, question, answer) VALUES (%s, %s, %s, %s)", (user, topic, q, a))
+            cur.execute("INSERT INTO qa (username, topic, question, answer) VALUES (%s, %s, %s, %s)", (username, topic, q, a))
         except Error as e:
             print(f"An error occurred: {e}")
             conn.rollback()
@@ -54,20 +54,22 @@ def new_qa(user:str, topic:str, qa_list: list) -> bool:
     conn.close()
     return None
 
-def new_flashcard(user:str, topic:str, flashcard_list: list) -> Error:
+def new_flashcard(username:str, topic:str, flashcard_list: list) -> Error:
     conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, sslmode=sslmode)
     cur = conn.cursor()
-
+    print("established connection")
     for flashcard in flashcard_list:
-        term = flashcard["term"]
-        definition = flashcard["definition"]
+        print(flashcard)
+        term = flashcard["question"]
+        definition = flashcard["answer"]
         try:
-            cur.execute("INSERT INTO flashcard (username, topic, term, definition) VALUES (%s, %s, %s, %s)", (user, topic, term, definition))
+            cur.execute("INSERT INTO flashcards (username, topic, term, def) VALUES (%s, %s, %s, %s)", (username, topic, term, definition))
         except Error as e:
             print(f"An error occurred: {e}")
             conn.rollback()
             return Error
 
+    print("done")
     conn.commit()
     cur.close()
     conn.close()
